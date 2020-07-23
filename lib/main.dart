cabd85b94faa2e649e68769b4fe8d9f38f3a1365
sseklecov <p0o9i8u7!>
2020-07-23 22:18:44 +0700

    Flutter sign in ui. Part 1

---
+++ lib/main.dart
@@ -0,0 +1,176 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: LoginPage(),
+    );
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomPaint(
+        painter: BackgroundSignIn(),
+        child: Stack(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 35),
+              child: Column(
+                children: <Widget>[
+                  _getHeader(),
+                  _getInputs(),
+                  _getSignIn(),
+                  _getBottomRow(),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+_getHeader() {
+  return Expanded(
+    flex: 3,
+    child: Container(
+      alignment: Alignment.bottomLeft,
+      child: Text(
+        'Добро пожаловать',
+        style: TextStyle(color: Colors.white, fontSize: 37),
+      ),
+    ),
+  );
+}
+
+_getInputs() {
+  return Expanded(
+    flex: 4,
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: <Widget>[
+        TextField(
+          decoration: InputDecoration(labelText: 'Логин'),
+        ),
+        SizedBox(
+          height: 15,
+        ),
+        TextField(
+          decoration: InputDecoration(labelText: 'Пароль'),
+        ),
+        SizedBox(
+          height: 15,
+        ),
+      ],
+    ),
+  );
+}
+
+_getSignIn() {
+  return Expanded(
+    flex: 1,
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Text(
+          'Вход',
+          style: TextStyle(fontSize: 25, fontWeight: FontWeight.w500),
+        ),
+        CircleAvatar(
+          backgroundColor: Colors.grey.shade800,
+          radius: 40,
+          child: Icon(
+            Icons.arrow_forward_ios,
+            color: Colors.white,
+          ),
+        )
+      ],
+    ),
+  );
+}
+
+_getBottomRow() {
+  return Expanded(
+    flex: 1,
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Text(
+          'Регистрация',
+          style: TextStyle(
+              fontSize: 15,
+              fontWeight: FontWeight.w500,
+              decoration: TextDecoration.underline),
+        ),
+        Text(
+          'Забыли пароль',
+          style: TextStyle(
+              fontSize: 15,
+              fontWeight: FontWeight.w500,
+              decoration: TextDecoration.underline),
+        )
+      ],
+    ),
+  );
+}
+
+class BackgroundSignIn extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    var sw = size.width;
+    var sh = size.height;
+    var paint = Paint();
+
+    Path mainBackground = Path();
+    mainBackground.addRect(Rect.fromLTRB(0, 0, sw, sh));
+    paint.color = Colors.grey.shade100;
+    canvas.drawPath(mainBackground, paint);
+
+    // Blue
+    Path blueWave = Path();
+    blueWave.lineTo(sw, 0);
+    blueWave.lineTo(sw, sh * 0.5);
+    blueWave.quadraticBezierTo(sw * 0.5, sh * 0.45, sw * 0.2, 0);
+    blueWave.close();
+    paint.color = Colors.blue.shade300;
+    canvas.drawPath(blueWave, paint);
+
+    // Grey
+    Path greyWave = Path();
+    greyWave.lineTo(sw, 0);
+    greyWave.lineTo(sw, sh * 0.1);
+    greyWave.cubicTo(
+        sw * 0.95, sh * 0.15, sw * 0.65, sh * 0.15, sw * 0.6, sh * 0.38);
+    greyWave.cubicTo(sw * 0.52, sh * 0.52, sw * 0.05, sh * 0.45, 0, sh * 0.4);
+    greyWave.close();
+    paint.color = Colors.grey.shade800;
+    canvas.drawPath(greyWave, paint);
+
+    // Yellow
+    Path yellowWave = Path();
+    yellowWave.lineTo(sw * 0.7, 0);
+    yellowWave.cubicTo(
+        sw * 0.6, sh * 0.05, sw * 0.27, sh * 0.01, sw * 0.18, sh * 0.12);
+    yellowWave.quadraticBezierTo(sw * 0.12, sh * 0.2, 0, sh * 0.2);
+    yellowWave.close();
+    paint.color = Colors.orange.shade300;
+    canvas.drawPath(yellowWave, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return oldDelegate != this;
+  }
+}
